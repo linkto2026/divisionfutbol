94e8016e6b3cf4b864841ad275647080cc04fcd8
linkto2026 <alejandrocespedes023@gmail.com>
2026-01-28 12:24:12 -0400

    Update categories.js

--- src/_data/categories.js
+++ src/_data/categories.js
@@ -2,9 +2,12 @@ module.exports = [
   { name: "Selección", slug: "seleccion" },
   { name: "División Profesional", slug: "division-profesional" },
   { name: "Copa Simón Bolívar", slug: "copa-simon-bolivar" },
+
+  // ACF (subcategorías)
   { name: "ACF Primera A", slug: "acf-primera-a" },
   { name: "ACF Primera B", slug: "acf-primera-b" },
   { name: "ACF Ascensos", slug: "acf-ascensos" },
+
   { name: "Internacional", slug: "internacional" },
   { name: "Automovilismo", slug: "automovilismo" },
   { name: "Otros Deportes", slug: "otros-deportes" }
