f3d74d5666a4bbe98dc3fdb142375555018acc72
linkto2026 <alejandrocespedes023@gmail.com>
2026-06-23 23:44:12 +0000

    Limpiar Tina y agregar Keystatic

--- tina/__generated__/client.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types.js";
-export const client = createClient({ url: "http://localhost:4001/graphql", token: "5d2f3ca5781a7ae50abe26ea9b489da2026109e5", queries });
-export default client;
